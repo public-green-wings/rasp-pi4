e8db67e7498c63e9269bbe597970d8979148c4cd
Chanbyeongee <well87865@gmail.com>
2022-11-21 16:43:21 +0900

    fix

--- src/pub_move_drone_node.cpp
+++ src/pub_move_drone_node.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <std_msgs/String.h> 
+#include <std_msgs/String.h>
 #include <stdio.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
@@ -28,7 +28,7 @@ void state_cb(const mavros_msgs::State::ConstPtr msg){
 void target_cb(const geographic_msgs::GeoPoseStamped::ConstPtr msg){
     target_pose = *msg;
     //ROS_INFO("MOVE!\n");
-    //ROS_INFO("Received(lat,long,alt): %4.2f, %4.2f, %4.2f\n",msg.pose.position.latitude, msg.pose.position.longitude, msg.pose.position.altitude);
+    ROS_INFO("Received(lat,long,alt): %4.2f, %4.2f, %4.2f\n",msg.pose.position.latitude, msg.pose.position.longitude, msg.pose.position.altitude);
 }
 /*
 void target_cb(const geometry_msgs::PoseStamped::ConstPtr msg){
@@ -40,7 +40,7 @@ void target_cb(const geometry_msgs::PoseStamped::ConstPtr msg){
 
 void global_cb(const sensor_msgs::NavSatFix msg ){
 	current_pose = msg;
-	//ROS_INFO("Current(lat,long,alt): %4.2f, %4.2f, %4.2f\n",msg.latitude, msg.longitude, msg.altitude);
+	ROS_INFO("Current(lat,long,alt): %4.2f, %4.2f, %4.2f\n",msg.latitude, msg.longitude, msg.altitude);
 }
 
 int main(int argc, char **argv)
@@ -53,19 +53,19 @@ int main(int argc, char **argv)
    ros::Publisher move_pub = n.advertise <geographic_msgs::GeoPoseStamped> ("mavros/setpoint_position/global", 10);
    //ros::Publisher move_pub = n.advertise <geometry_msgs::PoseStamped> ("mavros/setpoint_position/local", 10); //for local
    //ros::Publisher rotate_pub = n.advertise <mavros_msgs::PositionTarget> ("mavros/setpoint_raw/local", 10);
-   
+
    //subscribers
    ros::Subscriber target_sub = n.subscribe<geographic_msgs::GeoPoseStamped>("targeting",10,target_cb); //for global
    //ros::Subscriber target_sub = n.subscribe<geometry_msgs::PoseStamped>("targeting",10,target_cb);
    ros::Subscriber state_sub = n.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
    ros::Subscriber global_sub = n.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 10, global_cb);
-    
+
    //servicesClient
    ros::ServiceClient arming_client = n.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
    ros::ServiceClient set_mode_client = n.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
-   
+
    ros::Rate rate(20.0);
-   
+
     while(ros::ok() && !current_state.connected){
         ros::spinOnce();
         rate.sleep();
@@ -80,18 +80,18 @@ int main(int argc, char **argv)
 		//target_pose.pose.position.y = 0;
 		//target_pose.pose.position.z = 560;
 	}
-	
+
 	//rotate_pose.yaw = 0;
 	//rotate_pose.yaw_rate = 1;
-	
-	
+
+
 	//target_pose.latitude = current_pose.latitude;
     //target_pose.longitude = current_pose.longitude;
     //target_pose.altitude = current_pose.altitude + 5;
-    
+
     //alt_pose.pose.position.altitude = current_pose.altitude + 5;
-	
-    
+
+
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
         move_pub.publish(target_pose);
@@ -102,18 +102,18 @@ int main(int argc, char **argv)
 
     //mavros_msgs::SetMode offb_set_mode;
     //offb_set_mode.request.custom_mode = "OFFBOARD";
-	
+
     //mavros_msgs::CommandBool arm_cmd;
     //arm_cmd.request.value = true;
 
    ros::Time last_request = ros::Time::now();
-	
+
    while(ros::ok()){
        target_pose.header.stamp = ros::Time::now();
        //rotate_pose.header.stamp = ros::Time::now();
        target_pose.header.frame_id = 1;
        //rotate_pose.header.frame_id = 1;
-       
+
        /*
        if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
@@ -122,7 +122,7 @@ int main(int argc, char **argv)
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
-            
+
              for(int i = 100; ros::ok() && i > 0; --i){
 				move_pub.publish(target_pose);
 				//alt_pub.publish(alt_pose);
@@ -140,22 +140,21 @@ int main(int argc, char **argv)
 				}
             }
 		else{
-		
+
 		   move_pub.publish(target_pose);
 		   //rotate_pub.publish(rotate_pose);
 			//alt_pub.publish(alt_pose);
         }
         */
-        
+
 	   //count+=0.01;
 	   //rotate_pose.yaw=count;
-	   
+
        move_pub.publish(target_pose);
-       
+
        ros::spinOnce();
        rate.sleep();
 
    }
    return 0;
 }
-
