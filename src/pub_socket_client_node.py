e85d3fdad063c7223673072c7dea8cb83f58f34a
Chanbyeongee <well87865@gmail.com>
2022-11-23 23:40:09 +0900

    fix

--- src/pub_socket_client_node.py
+++ src/pub_socket_client_node.py
@@ -54,6 +54,7 @@ if __name__ == "__main__" :
 	sio.connect(host,namespaces=['/realtime'],wait_timeout=15)
 
 	while not rospy.is_shutdown():
+		rospy.loginfo("EMIT!")
 		sio.emit("CUR_POS",{"lat":global_msg.latitude, "long":global_msg.longitude, "alt":global_msg.altitude},namespace='/realtime')
 		sio.sleep(0)
 		rospy.spin()
