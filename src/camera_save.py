1b71c5e639bb5f9fa3d225181e2b3fde33f34a25
Chanbyeongee <well87865@gmail.com>
2022-11-26 02:18:09 +0900

    modify:camera

--- src/camera_save.py
+++ src/camera_save.py
@@ -5,7 +5,9 @@ capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 videoWriter = cv2.VideoWriter('video.avi', fourcc, 30.0, (640,480))
  
-while (True):
+frames = 10000
+
+while (frames>0):
  
     ret, frame = capture.read()
      
@@ -15,7 +17,10 @@ while (True):
  
     if cv2.waitKey(1) == 27:
         break
- 
+    
+    frames -= 1
+    print("left frames",frames)
+
 capture.release()
 videoWriter.release()
  
