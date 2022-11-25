4e143827eda6548cd89a0f2420e84e65ab8bc643
Chanbyeongee <well87865@gmail.com>
2022-11-26 02:22:50 +0900

    fix

--- src/camera_save.py
+++ src/camera_save.py
@@ -2,8 +2,8 @@ import cv2
  
 capture = cv2.VideoCapture(0)
  
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-videoWriter = cv2.VideoWriter('video.avi', fourcc, 30.0, (640,480))
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+videoWriter = cv2.VideoWriter('video.mp4', fourcc, 30.0, (640,480))
  
 frames = 50
 
@@ -17,7 +17,7 @@ while (frames>0):
  
     if cv2.waitKey(1) == 27:
         break
-    
+
     frames -= 1
     print("left frames",frames)
 
