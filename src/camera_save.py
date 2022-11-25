416422b6e3095d9f48273421d339cd27211f21a2
Chanbyeongee <well87865@gmail.com>
2022-11-26 02:55:38 +0900

    fix

--- src/camera_save.py
+++ src/camera_save.py
@@ -4,13 +4,14 @@ capture = cv2.VideoCapture(0)
 
 fps = capture.get(cv2.CAP_PROP_FPS)
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-videoWriter = cv2.VideoWriter('video.avi', fourcc, fps, (int(capture.get(3)),int(capture.get(4))))
+videoWriter = cv2.VideoWriter('video.avi', fourcc, 5, (int(capture.get(3)),int(capture.get(4))))
  
 frames = 30
 
 while (frames>0):
  
     ret, frame = capture.read()
+    print(len(frame))
      
     if ret:
 #        cv2.imshow('video', frame)
